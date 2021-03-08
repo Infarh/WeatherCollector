b160758d311e43fe0ff1f2e703f314e1897838da
Infarh <shmachilin@yandex.ru>
2021-03-08 11:05:39 +0300

    Структура проектов

---
+++ Tests/WeatherCollector.Services.Tests/StubTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WeatherCollector.Services.Tests
+{
+    [TestClass]
+    public class StubTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+        }
+    }
+}
